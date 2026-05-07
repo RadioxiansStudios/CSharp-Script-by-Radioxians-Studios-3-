1f41575c29f4a3b89ee85ec44ef3683ca3d80a24
Radioxians Studios <gjonocagjeikov@gmail.com>
2026-05-07 17:10:29 +0200

    Add files via upload

---
+++ Tutorial4.cs
@@ -0,0 +1,29 @@
+// © 2026 Radioxians Studios. All Rights Reserved.
+// v 1.0
+// You can study, observe, and do whatever you want on this script
+// This script is on Git Hub
+
+using UnityEngine;
+
+public class Tutorial4 : MonoBehaviour
+{
+
+    public float potenzaSalto = 10;
+    public float velocità = 5;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("JOINED");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("You are going forward");
+        }
+    }
+}
